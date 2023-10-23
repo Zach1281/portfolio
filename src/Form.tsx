a59edcafed6c0ff4f809b2664766a1018a316d1a
Zach128 <zachary_romero@yahoo.com>
2023-10-23 12:59:38 -0700

    Remove contact form, replace with link to email

--- src/Form.tsx
+++ src/Form.tsx
@@ -4,35 +4,9 @@ function Form() {
   return (
     <>
       <div className="form-container">
-        <form action="#" className="contact">
-          <div className="name-container">
-            <label htmlFor="name">Name </label>
-            <input type="text" name="name" id="name" placeholder="Your name" />
-          </div>
-          <div className="email-container">
-            <label htmlFor="email">Email </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="your@email.com"
-            />
-          </div>
-          <div className="comment-container">
-            <label htmlFor="comment">Comment </label>
-            <textarea
-              name="comment"
-              rows={4}
-              cols={50}
-              placeholder="Enter your comment"
-            />
-          </div>
-          <div className="button-container">
-            <button type="submit" value="contact">
-              Submit
-            </button>
-          </div>
-        </form>
+        <a href="mailto:zacharyromerodev@gmail.com">
+          <img src="../email-svgrepo-com.svg" alt="Email Link" />
+        </a>
       </div>
     </>
   );
